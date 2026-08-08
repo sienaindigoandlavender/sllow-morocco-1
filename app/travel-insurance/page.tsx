55ce865647a4b2b0858b7bac2fd90b28c49a4be7
sienaindigoandlavender <siena@indigoandlavender.love>
2026-08-08 12:30:39 +0100

    Add files via upload

--- app/travel-insurance/page.tsx
+++ app/travel-insurance/page.tsx
@@ -8,6 +8,10 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "https://www.slowmorocco.com/travel-insurance",
   },
+  robots: {
+    index: false,
+    follow: true,
+  },
 };
 
 export default function TravelInsurancePage() {
